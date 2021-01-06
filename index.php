23386ea5c7874943b5cf6a4524d3438c9f2d6f08
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-01-06 18:01:07 +0100

    THE-DREAM: Creating a form that gets all the values I needed Making the array with all the values visible to make it easier Connect all the variables to the inputs Make all the if statements to calculate the differences between the valuta

--- index.php
+++ index.php
@@ -15,48 +15,133 @@
     </header>
 
     <div class="box-container">
-        <form action="">
+        <?php
+            
+            if (isset($_POST['submit'])) {
+                $amount = $_POST['originalInput'];
+                $cur1 = $_POST['country01'];
+                $cur2 = $_POST['country02'];
+                $newCur = $_POST['newValue'];
+
+                if($cur1=="€" AND $cur2=="€"){
+                    $newCur = $amount;
+                }
+
+                if($cur1=="$" AND $cur2=="$"){
+                    $newCur = $amount;
+                }
+
+                if($cur1=="£" AND $cur2=="£"){
+                    $newCur = $amount;
+                }
+
+                if($cur1=="RUB" AND $cur2=="RUB"){
+                    $newCur = $amount;
+                }
+                
+                if($cur1=="€" AND $cur2=="$"){
+                    $newCur = $amount*1.23;
+                }
+
+                if($cur1=="€" AND $cur2=="£"){
+                    $newCur = $amount*0.90;
+                }
+
+                if($cur1=="€" AND $cur2=="RUB"){
+                    $newCur = $amount*90.76;
+                }
+
+                if($cur1=="$" AND $cur2=="€"){
+                    $newCur = $amount*0.81;
+                }
+
+                if($cur1=="$" AND $cur2=="£"){
+                    $newCur = $amount*0.74;
+                }
+
+                if($cur1=="$" AND $cur2=="RUB"){
+                    $newCur = $amount*73.89;
+                }
+
+                if($cur1=="£" AND $cur2=="€"){
+                    $newCur = $amount*1.11;
+                }
+
+                if($cur1=="£" AND $cur2=="$"){
+                    $newCur = $amount*1.36;
+                }
+
+                if($cur1=="£" AND $cur2=="RUB"){
+                    $newCur = $amount*100.41;
+                }
+
+                if($cur1=="RUB" AND $cur2=="€"){
+                    $newCur = $amount*0.011;
+                }
+
+                if($cur1=="RUB" AND $cur2=="$"){
+                    $newCur = $amount*0.014;
+                }
+
+                if($cur1=="RUB" AND $cur2=="£"){
+                    $newCur = $amount*0.0100;
+                }
+
+            }
+        ?>
+
+        <form action="" method="POST">
             <div class="countries">
                 <div class="country-origin country-select">
                     <label for="countryOrigin">Select a country:</label>
                     <select id="countryOrigin" name="country01">
-                        <option value="BE">Belgium</option>
-                        <option value="US">United States</option>
-                        <option value="UK">United Kingdom</option>
-                        <option value="RU">Russia</option>
+                        <option value="€">Belgium</option>
+                        <option value="$">United States</option>
+                        <option value="£">United Kingdom</option>
+                        <option value="RUB">Russia</option>
                     </select>
                 </div>
 
                 <div class="switch-countries">
-                    <button id="switchCountries">switch!</button>
+                    <button id="switchCountries" disabled>switch!</button>
                 </div>
 
                 <div class="country-new country-select">
                     <label for="countryNew">Select another country:</label>
                     <select id="countryNew" name="country02">
-                        <option value="BE">Belgium</option>
-                        <option value="US">United States</option>
-                        <option value="UK">United Kingdom</option>
-                        <option value="RU">Russia</option>
+                        <option value="€">Belgium</option>
+                        <option value="$">United States</option>
+                        <option value="£">United Kingdom</option>
+                        <option value="RUB">Russia</option>
                     </select>
                 </div>
             </div>
 
             <div class="values">
-                <input type="text" placeholder="Your amount" id="inputValue" name="originalInput">
-                <input type="text" readonly id="currency01" class="currency-types" name="currency01">
+                <input type="text" placeholder="Your amount" autocomplete="on" id="inputValue" name="originalInput" value="<?php echo $amount?>">
+                <input type="text" readonly id="currency01" class="currency-types" name="currency01" value="<?php echo $cur1?>">
 
                 <p><span>=</span></p>
 
-                <input type="text" placeholder="Result" id="newValue" name="newValue">
-                <input type="text" readonly id="currency02" class="currency-types" name="currency02">
+                <input type="text" readonly placeholder="Result" id="newValue" name="newValue" value="<?php echo $newCur?>">
+                <input type="text" readonly id="currency02" class="currency-types" name="currency02" value="<?php echo $cur2?>">
             </div>
 
             <div class="calculate">
-                <button id="calculate">Go!</button>
+                <input id="submit" type="submit" name="submit" value="Go!">
             </div>
         </form>
     </div>
     
 </body>
 </html>
+
+<?php
+
+function pre_r( $array ) {
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+}
+
+?>
